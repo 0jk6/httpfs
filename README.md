f6542820339cd0b6f05e35f75b7e1049d30735d3
jaychandra6 <jaypatel6814@gmail.com>
2022-02-07 16:21:31 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# httpfs - browse and download files over local network
+
+`httpfs` will run on any device that supports NodeJS, you can browse and download files over http
+
+use `npm install` and then `npm start` to start the server. To start browsing files, you need to goto your `local_ip:3000`
---
+++ server.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+const express = require("express");
+const { path } = require("express/lib/application");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res)=>{
+    res.render("index.ejs");
+});
+
+
+app.get("/files", (req, res)=>{
+
+    let path = req.query.path;
+    
+    fileList = [];
+
+
+    if(path == "" || path == null){
+        res.render("files.ejs", {path : path, data : [], err : "not a valid directory"})
+    }
+    else{
+        try{
+            fileList = fs.readdirSync(path)
+        }
+        catch(err){
+            console.log("cannot find the directory");
+            res.render("files.ejs", {path : path, data : [], err : "not a valid directory"});
+            return;
+        }
+    }
+
+    try{
+        res.render("files.ejs", {path : path, data : fileList, err : ""});
+    }
+    catch(err){
+        console.log("ERROR in /files route");
+    }
+});
+
+app.get("/download", (req, res)=>{
+    let filePath = req.query.path;
+
+    res.download(filePath);
+
+})
+
+app.listen(PORT, ()=>{
+    console.log(`listening on port ${PORT}`);
+});
